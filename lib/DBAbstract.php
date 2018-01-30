b9ee5740a42d572ce728907ea24ba22c158863f5
erzh <cmpan@qq.com>
2018-01-30 14:25:37 +0800

    数据库字段名不转成小写

--- lib/DBAbstract.php
+++ lib/DBAbstract.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Windwork
- * 
+ *
  * 一个用于快速开发高并发Web应用的轻量级PHP框架
- * 
+ *
  * @copyright Copyright (c) 2008-2017 Windwork Team. (http://www.windwork.org)
  * @license   http://opensource.org/licenses/MIT
  */
@@ -11,14 +11,14 @@ namespace wf\db;
 
 /**
  * 数据库操作抽象类
- *  
+ *
  * @package     wf.db
  * @author      cm <cmpan@qq.com>
  * @link        http://docs.windwork.org/manual/wf.db.html
  * @since       0.1.0
  */
 abstract class DBAbstract implements DBInterface
-{    
+{
     /**
      * 数据库连接配置
      * @var array
@@ -37,23 +37,23 @@ abstract class DBAbstract implements DBInterface
      * @var int
      */
     protected $rollbackAgain = false;
-    
+
     /**
      * 记录当前请求执行的SQL查询语句
      * @var array
      */
     protected $log = [];
-            
+
     /**
      * 数据库当前页面连接次数,每次实行SQL语句的时候 ++
-     * 
+     *
      * @var int
      */
     public $execTimes = 0;
-        
+
     /**
      * 获取模型数据表信息
-     * 
+     *
      * <pre>
      * arry(
      *     'pk' => '主键',
@@ -83,31 +83,31 @@ abstract class DBAbstract implements DBInterface
     public function getTableSchema($table)
     {
         static $tableSchemaList = [];
-        
+
         if((!$tableSchemaList || empty($tableSchemaList[$table]))) {
             $rows = $this->getAll("SHOW COLUMNS FROM %t", [$table]);
             $tableSchema = array(
-                'pk'      => '', 
-                'ai'      => false, 
+                'pk'      => '',
+                'ai'      => false,
                 'field'   => []
             );
             foreach ($rows as $row) {
-                $tableSchema['field'][strtolower($row['Field'])] = $row;
-                
+                $tableSchema['field'][$row['Field']] = $row;
+
                 if ($row['Key'] == 'PRI') {
                     if($tableSchema['pk']) {
                         $tableSchema['pk'] = (array)$tableSchema['pk'];
-                        $tableSchema['pk'][] = strtolower($row['Field']);
+                        $tableSchema['pk'][] = $row['Field'];
                     } else {
                         $tableSchema['ai'] = $row['Extra'] == 'auto_increment';
-                        $tableSchema['pk'] = strtolower($row['Field']);
+                        $tableSchema['pk'] = $row['Field'];
                     }
                 }
             }
-            
-            $tableSchemaList[$table] = $tableSchema;            
+
+            $tableSchemaList[$table] = $tableSchema;
         }
-        
+
         return $tableSchemaList[$table];
     }
 
@@ -124,30 +124,30 @@ abstract class DBAbstract implements DBInterface
     public function insertRows(array $rows, $table, $fieldArr = [], $isReplace = false)
     {
         $type = $isReplace ? 'REPLACE' : 'INSERT';
-        
+
         // 数据中允许插入的字段
         $allowFields = $fieldArr ? $fieldArr : array_keys(current($rows));
         $allowFields = QueryBuilder::quoteFieldNames(implode(',', $allowFields));
-        
-        // 
+
+        //
         $valueArr = [];
         foreach ($rows as $row) {
             $rowStr = '';
             foreach ($row as $key => $val) {
                 // 去掉不允许写入的属性
-                if ($fieldArr && !in_array(strtolower($key), $fieldArr)) {
+                if ($fieldArr && !in_array($key, $fieldArr)) {
                     unset($row[$key]);
                 }
             }
-            
+
             $rowStr = implode(',', array_map('\wf\db\QueryBuilder::quoteInputVar', $row));
             $valueArr[] = "({$rowStr})";
         }
         $values = $rowStr = implode(',', $valueArr);
-        
+
         return $this->exec("%x INTO %t (%x) VALUES %x", array($type, $table, $allowFields, $values));
     }
-    
+
     /**
      * 构筑函数设置配置信息
      * @param array $cfg
